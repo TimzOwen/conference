5434b17795848fe7868e72fba42b07d44627f16c
TimzOwen <ktimothy@kabarak.ac.ke>
2021-04-08 14:14:26 +0300

    url fix

--- src/main/java/com/codewithtimzowen/conference/model/Registration.java
+++ src/main/java/com/codewithtimzowen/conference/model/Registration.java
@@ -1,4 +1,13 @@
 package com.codewithtimzowen.conference.model;
 
 public class Registration {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Registration Page here</h1>
-    <form:form>
+    <form:form modelAttribute="registration">
         <table>
             <tr>
                 <td>
